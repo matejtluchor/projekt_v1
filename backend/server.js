7c7c2571c04742025690f84821ec90a0c6522d22
Matej <matej.tluchor460@gmail.com>
2025-12-09 17:03:09 +0100

    Oprava portu pro Render

--- backend/server.js
+++ backend/server.js
@@ -336,4 +336,8 @@ app.get("/api/admin/stats/day", (req, res) => {
   );
 });
 
-app.listen(3000, () => console.log("✅ Backend běží na http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("✅ Backend běží na portu " + PORT);
+});
